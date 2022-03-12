7f17483dd29058ee43d7b838fe1b063c253204a0
Nikolas Mazur <kolemazur@gmail.com>
2022-03-11 20:56:20 -0700

    Rework user input

--- script.js
+++ script.js
@@ -18,11 +18,16 @@ generateBtn.addEventListener("click", writePassword);
 const lowerCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const upperCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P","Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const numberCharacters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const symbolCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "+", "=", "//"];
+const symbolCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "+", "=", "\\"];
 
 // User input
-const userLength = promt("Length of desired password to password");
-const userLower = confirm("Add lower case characters to password?");
-const userUpper = confirm("Add upper case characters to password?");
-const userNum = confirm("Add numbers to password?");
-const userSymbol = confirm("Add symbols to password?")
+passwordLength = prompt("Length of password; between 8 and 128 characters.");
+if (passwordLength < 8 || passwordLength > 128) {
+  return "Must be between 8 and 128 characters.";
+} else if (isNaN(passwordLength)) {
+  passwordLength = prompt("Must be a number.");
+} else {
+  alert("Generating " + passwordLength + " character long password.");
+}
+
+//Random character select

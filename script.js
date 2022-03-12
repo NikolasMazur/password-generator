3288e4c38e3a352e5cfa7dafbaf37ebfe188ac62
Nikolas Mazur <kolemazur@gmail.com>
2022-03-12 01:32:59 -0700

    Change collections constants to variables

--- script.js
+++ script.js
@@ -16,10 +16,10 @@ generateBtn.addEventListener("click", writePassword);
 
 // Character collections
 function generatePassword() {
-  const lowerCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  const upperCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P","Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  const numberCharacters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  const symbolCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "+", "=", "\\"];
+  var lowerCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var upperCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P","Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var numberCharacters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var symbolCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "+", "=", "\\"];
 
 // User input
   passwordLength = prompt("Length of password; between 8 and 128 characters.");
